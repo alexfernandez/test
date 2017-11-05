3d9d44cb0d7f2988ede1bfded42bed4172ad4cdc
Alex Fernández <alexfernandeznpm@gmail.com>
2017-11-06 00:34:13 +0100

    Test files.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.test = function() {
+  console.log('Everything is fine');
+  process.exit(0);
+}
+
+console.log('Running integration demo');
+
---
+++ test.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const index = require('./index.js');
+
+index.test();
+
